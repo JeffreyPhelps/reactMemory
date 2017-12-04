2e8dc44aa7a522d106c2e73004932a84c965d5f7
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-03 22:18:48 -0700

    added component initial structure, and created Navbar component

---
+++ react_memory/src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+
+
+function Navbar() {
+	return(
+	<div class="navbar-fixed">
+		<nav>
+			<div class="nav-wrapper container">
+				<a href='#modal1' className='left'>Instructions</a>
+				<a href="#" className='brand-logo center'>Memory Game</a>
+				<ul className='right'>
+					<li style={{paddingRight: "20px"}}>Score: </li>
+					<li style={{paddingLeft: "20px"}}>Top Score: </li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+	)
+}
+
+
+
+export default Navbar;
